e201eb4b42ab67f68622bcb6d235e339217ea8f8
Lawrence Chen <lawrencechen98@gmail.com>
2018-05-20 20:56:49 -0700

    Working preprocess code

--- preprocess.py
+++ preprocess.py
@@ -8,21 +8,20 @@ def format_path(path):
         path = path[:-1]
     return path
 
-def preprocess(path, path_to_save, thresh_amt):
+def preprocess(path, path_to_save):
     path = format_path(path)
     path_to_save = format_path(path_to_save)
     pic_names = os.listdir(path)
     pics = ['{}/{}'.format(path,pic_name) for pic_name in pic_names]
-    pics = ['test_pic2.jpg']
     for pic,pic_name in zip(pics,pic_names):
         img = cv2.imread(pic, 0)
-        ret, threshed = cv2.threshold(img,thresh_amt,255,cv2.THRESH_BINARY)
+        resized = cv2.resize(img, (720,720))
+        blurred = cv2.medianBlur(resized, 5)
+        threshed = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,int(len(img)/20)*2-1,11)
         ind = pic_name.index('.')
-        resized = cv2.resize(threshed, (720,720))
-        cv2.imwrite('{}/{}.png'.format(path_to_save,pic_name[:ind]), resized)
+        cv2.imwrite('{}/{}.png'.format(path_to_save,pic_name[:ind]), threshed)
 
 if(__name__ == '__main__'):
     path = sys.argv[1]
     path_to_save = sys.argv[2]
-    thresh_amt = 50
-    preprocess(path, path_to_save, thresh_amt)
+    preprocess(path, path_to_save)
