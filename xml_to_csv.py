dc35efc1d90fe321cc9e513320f017da444f99a7
henry yang <hyang97@gmail.com>
2018-05-29 22:45:48 -0700

    with csv

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -26,9 +26,9 @@ def xml_to_csv(path):
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
+    image_path = os.path.join(os.getcwd(), 'annotations_1')
     xml_df = xml_to_csv(image_path)
-    xml_df.to_csv('raccoon_labels.csv', index=None)
+    xml_df.to_csv('labels_1.csv', index=None)
     print('Successfully converted xml to csv.')
 
 
